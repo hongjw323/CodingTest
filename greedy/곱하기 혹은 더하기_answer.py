8aa85e32c94e47de1f2fc85b0f823ee0074cdd14
hongjw323 <hbinibing23@gmail.com>
2021-09-17 08:44:29 +0900

    0917_곱하기 혹은 더하기

---
+++ greedy/곱하기 혹은 더하기_answer.py
@@ -0,0 +1,13 @@
+data = input()
+
+result = int(data[0])
+
+for i in range(1,len(data)):
+    num = int(data[i])
+
+    if result <= 1 or num <= 1:
+        result += num
+    else:
+        result *= num
+
+print(result)
