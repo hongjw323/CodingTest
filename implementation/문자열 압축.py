1722a4ffe2189d86edfb6953c54870dc2d26297f
hongjw323 <hbinibing23@gmail.com>
2022-04-30 18:42:24 +0900

    0429_구현

--- implementation/문자열 압축.py
+++ implementation/문자열 압축.py
@@ -1,2 +1,26 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60057
 
+def solution(s):
+    unit = len(s) // 2
+    answer = len(s)
+    for i in range(1,unit+1):
+        cnt = 1
+        compressed = ""
+        part = s[0:i]
+        for j in range(i,len(s),i):
+            if part == s[j:j+i]:
+                cnt += 1
+            else:
+                if cnt > 1:
+                    compressed += str(cnt) + part
+                else:
+                    compressed += part
+                cnt = 1
+                part = s[j:j+i]
+        if cnt > 1:
+            compressed += str(cnt) + part
+        else:
+            compressed += part
+        answer = min(answer,len(compressed))
+        
+    return answer
